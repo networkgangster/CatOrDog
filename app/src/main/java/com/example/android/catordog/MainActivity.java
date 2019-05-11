ad40f8474c618016a4680d29978def3a1d13b4a9
networkgangster <abdussamed_koekdal@hotmail.de>
2019-05-11 16:28:09 +0200

    Initial Commit

--- app/src/main/java/com/example/android/catordog/MainActivity.java
+++ app/src/main/java/com/example/android/catordog/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.catordog;
 
 import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,15 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
-
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(getResources().getString(R.string.app_name));
-        Tablayout tabLayout = findViewById(R.id.tablayout);
+        TabLayout tabLayout = findViewById(R.id.tablayout);
         TabItem tabChats = findViewById(R.id.tabGalerie);
         TabItem tabStatus = findViewById(R.id.tabKamera);
         TabItem tabCalls = findViewById(R.id.tabStatistik);
+
+        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+
         ViewPager viewPager = findViewById(R.id.viewPager);
 
         viewPager.setAdapter(pageAdapter);
